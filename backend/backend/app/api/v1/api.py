77e3f3e4594cb7ac2dfae55351961795cbf543c4
clodobox <36227432+clodobox@users.noreply.github.com>
2025-08-28 08:34:59 +0200

    Delete backend directory

--- backend/backend/app/api/v1/api.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.v1.endpoints import equipment, projects, contacts
-
-api_router = APIRouter()
-
-api_router.include_router(equipment.router, prefix="/equipment", tags=["equipment"])
-api_router.include_router(projects.router, prefix="/projects", tags=["projects"])
-api_router.include_router(contacts.router, prefix="/contacts", tags=["contacts"])
